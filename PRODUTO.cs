e9ac251b24e08bc549fb2274fd57e669aae60142
natal <natal@NAT-85>
2025-04-22 19:49:38 -0300

    Adicionar arquivos de projeto.

---
+++ PRODUTO.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+public class Produto
+{
+    // Campos privados
+    private string nome;
+    private double preco;
+    private int quantidadetotal;
+
+    // Propriedades públicas com encapsulamento
+    public string Nome
+    {
+        get { return nome; }
+        set { nome = value; }
+    }
+
+    public double Preco
+    {
+        get { return preco; }
+        set { preco = value; }
+    }
+
+    // Propriedade de escrita para quantidade
+    public int Quantidade
+    {
+        set { AdicionarEstoque(value); }
+    }
+
+    // Propriedade de leitura para quantidadetotal
+    public int QuantidadeTotal
+    {
+        get { return quantidadetotal; }
+    }
+
+    // Método para adicionar ao estoque
+    public void AdicionarEstoque(int quantidade)
+    {
+        if (quantidade > 0)
+        {
+            quantidadetotal += quantidade;
+        }
+    }
+
+    // Método para remover do estoque
+    public void RemoverEstoque(int qtde)
+    {
+        if (qtde <= quantidadetotal)
+        {
+            quantidadetotal -= qtde;
+        }
+        else
+        {
+            Console.WriteLine("Erro: Estoque insuficiente para remover a quantidade solicitada.");
+        }
+    }
+
+    // Método que retorna o valor total em estoque
+    public double ValorTotalEmEstoque()
+    {
+        return preco * quantidadetotal;
+    }
+}
